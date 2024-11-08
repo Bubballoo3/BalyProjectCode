ff079c8c151937e6fe2892052b887cee9f88ca7e
Braeden <singleton1@kenyon.edu>
2024-11-08 14:39:39 -0500

    finsihed testing balyClasses

--- testing.rb
+++ testing.rb
@@ -17,15 +17,19 @@ class FileTester
 end
 
 #balyClasses.rb
-load 'balyClasses.rb'
+
 class BalyClassesTester < FileTester
+  load 'balyClasses.rb'
   def testAll()
     testClassExtensions
     testClassificationClass
+    testLocationClasses
     testSlideClass
+    testSubcollectionClass
     return true
   end
 
+private
   def testClassExtensions()
     # These methods should always work, 
     # so we define a universal error for all of them
@@ -109,6 +113,99 @@ class BalyClassesTester < FileTester
     classificationExpect(e.to_s,"B34.003")
     f=Classification.new("F. 42")
     classificationExpect(f.to_s,"F.042")
+    g=Classification.new("DF.2")
+    classificationExpect(g.to_s,"DF.002")
+
+    # Test group and number methods
+    classificationExpect(c.group, "B")
+    classificationExpect(c.number, 5)
+    classificationExpect(d.group, "CD")
+    classificationExpect(d.number, 55)
+    classificationExpect(e.group, "B34")
+    classificationExpect(e.number, 3)
+    classificationExpect(f.group, "F")
+    classificationExpect(f.number, 42)
+    classificationExpect(g.group, "DF")
+    classificationExpect(g.number, 2)
+
+    # Test sortingnumber method
+    classificationExpect(c.sortingNumber, 2005)
+    classificationExpect(d.sortingNumber, 82055)
+    classificationExpect(e.sortingNumber, 0)
+    classificationExpect(f.sortingNumber, 6042)
+    classificationExpect(g.sortingNumber, 110002)
+
+    # Test stringNum method
+    classificationExpect(c.stringNum, "005")
+    classificationExpect(d.stringNum, "055")
+    classificationExpect(e.stringNum, "003")
+    classificationExpect(f.stringNum, "042")
+    classificationExpect(g.stringNum, "002")
+
+    # Test classSystem method
+    classificationExpect(c.classSystem, "Baly")
+    classificationExpect(d.classSystem, "Baly")
+    classificationExpect(e.classSystem, "VRC")
+    classificationExpect(f.classSystem, "Baly")
+    classificationExpect(g.classSystem, "Baly")
+
+    # Test inRange?
+    classificationExpect(c.inRange?("C.001-100"), false)
+    classificationExpect(c.inRange?("C.006-10"),false)
+    classificationExpect(c.inRange?("B.001-100"), true)
+    classificationExpect(c.inRange?("B.006-10"),false)
+    classificationExpect(d.inRange?("CD.001-100"), true)
+    classificationExpect(d.inRange?("CD.80-100"), false)
+    classificationExpect(e.inRange?("B34.006-10"),false)
+    classificationExpect(e.inRange?("B34.001-100"), true)
+    classificationExpect(f.inRange?("F.040-80"), true)
+    classificationExpect(f.inRange?("F.006-10"),false)
+    classificationExpect(g.inRange?("DF.019-100"), false)
+    classificationExpect(g.inRange?("DF.006-10"),false)
+    classificationExpect(g.inRange?("DF.002"),true)
+
+  end
+
+  def locationExpect(value,expectation)
+    err=TestingError.new "A location method test has failed. Expected #{expectation} but got #{value}."
+    raise err unless value == expectation
+  end
+  def testLocationClasses()
+    # Test GeneralLocation
+    # Standard syntax
+    genloc=GeneralLocation.new([[0,0],"A place"])
+    locationExpect(genloc.name,"A place")
+    locationExpect(genloc.coords,[0,0])
+    locationExpect(genloc.notes,"")
+    # Nonstandard syntax
+    genloc2=GeneralLocation.new([[1,1],"Another place","Notes"])
+    locationExpect(genloc2.name,"Another place")
+    locationExpect(genloc2.coords,[1,1])
+    locationExpect(genloc2.notes,"Notes")
+    # Test SpecificLocation
+     # Standard syntax
+    specloc=SpecificLocation.new([[1,1],"possible location at 35 degrees NE","Some notes","Some medium title"])
+    locationExpect(specloc.title,"Some medium title")
+    locationExpect(specloc.coords,[1,1])
+    locationExpect(specloc.notes,"Some notes")
+    locationExpect(specloc.precision,"possible")
+    locationExpect(specloc.angle.degrees,35)
+    locationExpect(specloc.angle.direction,"NE")
+    locationExpect(specloc.angle.to_s,"35 degrees NE")
+    # Nonstandard syntax
+    specloc2=SpecificLocation.new([[2,2],"0 degrees N","Some medium notes","Some medium title"])
+    locationExpect(specloc2.title,"Some medium title")
+    locationExpect(specloc2.coords,[2,2])
+    locationExpect(specloc2.notes,"Some medium notes")
+    locationExpect(specloc2.precision,"exact")
+    locationExpect(specloc2.angle.degrees,0)
+    locationExpect(specloc2.angle.direction,"N")
+    locationExpect(specloc2.angle.to_s,"0 degrees N")
+    # Vertical Direction
+    specloc3=SpecificLocation.new( [[2,2],"estimated location facing up"])
+    locationExpect(specloc3.angle.direction,"UP")
+    locationExpect(specloc3.angle.degrees, -1)
+    locationExpect(specloc3.angle.to_s,"-1 degrees UP")
   end
   def slideExpect(value,expectation)
     err=TestingError.new "A slide method expected #{expectation} but got #{value}"
@@ -206,6 +303,55 @@ class BalyClassesTester < FileTester
     slideExpect(bSlide.specificLocation.angle.direction,mamplespecloc.angle.direction)
     slideExpect(bSlide.specificLocation.angle.to_s,mamplespecloc.angle.to_s)
   end
+  def subcollectionExpect(value,expectation)
+    err=TestingError.new "A subcollection method expected #{expectation} but got #{value}"
+    raise err unless value == expectation
+  end
+  def testSubcollectionClass()
+    #This class is intended to allow ranges between collections by 
+    #predicting what the next group of 100 will look like.
+
+    # VRC example
+    s1=Subcollection.new("B43.2")
+    subcollectionExpect(s1.group,"B43")
+    subcollectionExpect(s1.hundreds,"2")
+    subcollectionExpect(s1.to_s,"B43.2")
+    subcollectionExpect(s1.isVRC?,true)
+    #Test addone
+    s1.addone()
+    subcollectionExpect(s1.to_s, "B43.3")
+
+    # VRC edge case 
+    s2=Subcollection.new("B48.9")
+    subcollectionExpect(s2.group,"B48")
+    subcollectionExpect(s2.hundreds,"9")
+    subcollectionExpect(s2.to_s,"B48.9")
+    subcollectionExpect(s2.isVRC?,true)
+    #Test addone
+    s2.addone()
+    subcollectionExpect(s2.to_s, "B49.0")
+
+    # Baly Case
+    # simpler (no skipped)
+    s3=Subcollection.new("AB.0")
+    subcollectionExpect(s3.group,"AB")
+    subcollectionExpect(s3.hundreds,"0")
+    subcollectionExpect(s3.to_s,"AB.0")
+    subcollectionExpect(s3.isVRC?,false)
+    #Test addone
+    s3.addone()
+    subcollectionExpect(s3.to_s, "AC.0")
+
+    #harder case (Q collection doesn't exist)
+    s4=Subcollection.new("P.0")
+    subcollectionExpect(s4.group,"P")
+    subcollectionExpect(s4.hundreds,"0")
+    subcollectionExpect(s4.to_s,"P.0")
+    subcollectionExpect(s4.isVRC?,false)
+    #Test addone
+    s4.addone()
+    subcollectionExpect(s4.to_s, "R.0")
+  end
 end
 
 
@@ -221,7 +367,7 @@ class PrettyCommonFunctionsTester < FileTester
     testParseSlideRange
     return true
   end
-
+private
 # getCatType() 
 #   IN: a classification of a slide
 #   OUT: the system of this classification, either "VRC" or "Baly" or "N/A" (if not parseable)
